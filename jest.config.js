c30f29dbe59bb71126d98005524ef16575955f20
Roscoe Clark <roscoe.clark9@gmail.com>
2025-04-09 13:41:15 -0400

    jest configuration file

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,21 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jsdom",
+  collectCoverage: true, // Enable coverage collection
+  collectCoverageFrom: [
+    "!/**/*.test.tsx", // Exclude test files if needed
+    "!**/.next/**/*", // Exclude all files within the `.next` directory
+    "!**/.storybook/**/*",
+    "!**/stories/**/*",
+    "!**/coverage/**/*",
+    "!**/__tests__/**/*",
+    "!**/jest.config.js", // Exclude jest.config.js
+    "!**/next.config.js", // Exclude next.config.js
+    "!**/route.ts", // Exclude all route.ts files
+    "!**/_app.tsx", // Exclude all route.ts files
+  ],
+  coverageDirectory: "coverage", // Directory where the coverage report will be saved
+  coverageReporters: ["text", "lcov", "html"], // Include HTML coverage report
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
